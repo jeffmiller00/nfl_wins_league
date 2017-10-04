13333f06f7c2316b290eedbccf188b2b7d892302
Jeff Miller <jeffmiller00@gmail.com>
2017-10-04 10:21:51 -0700

    Updated weekly totals and small refactor of fetch script.

--- fetch_wins.rb
+++ fetch_wins.rb
@@ -19,13 +19,14 @@ def generate_team_table
 end
 
 EMPTY_WEEK = {jeff: 0, greg: 0, tim: 0, zach: 0, mike: 0}
+CURRENT_NFL_WEEK = 4
 
 def generate_summary_chart
   all_teams = get_teams
   weeklySummary = []
   week1begin = Date.parse('2017-09-07')
   week1end   = Date.parse('2017-09-11')
-  3.times do |i|
+  CURRENT_NFL_WEEK.times do |i|
     weeklySummary[i] = EMPTY_WEEK.dup
     weekBegin = week1begin.next_day(7*i)
     weekEnd   = week1end.next_day(7*i)
@@ -50,32 +51,34 @@ def generate_summary_chart
   end
 end
 
+def write_file?; false; end;
 
-request = HTTPI::Request.new
-request.url = 'http://www.nfl.com/standings'
-request.query = { category: 'league',
-                  season: '2017-REG',
-                  split: 'Overall',
-                  sort: 'OVERALL_WINS',
-                  order: 'desc'}
-response = HTTPI.get(request)
-full_doc = Nokogiri::HTML(response.body)
+if write_file?
+  request = HTTPI::Request.new
+  request.url = 'http://www.nfl.com/standings'
+  request.query = { category: 'league',
+                    season: '2017-REG',
+                    split: 'Overall',
+                    sort: 'OVERALL_WINS',
+                    order: 'desc'}
+  response = HTTPI.get(request)
+  full_doc = Nokogiri::HTML(response.body)
 
-truth_teams = full_doc.xpath('//*[@id="main-content"]/div[1]/div[2]/div[1]/table/tr')
+  truth_teams = full_doc.xpath('//*[@id="main-content"]/div[1]/div[2]/div[1]/table/tr')
 
-all_teams = File.read('./nfl_2017.json')
-all_teams = JSON.parse(all_teams)
+  all_teams = File.read('./nfl_2017.json')
+  all_teams = JSON.parse(all_teams)
 
-all_teams.each do |team|
-  wins = truth_teams.search("[text()*='#{team['name']}']").first.parent.parent.children[7].text.to_i
-  team['wins'][Date.today.prev_day.to_s] = wins
+  all_teams.each do |team|
+    wins = truth_teams.search("[text()*='#{team['name']}']").first.parent.parent.children[7].text.to_i
+    team['wins'][Date.today.prev_day.to_s] = wins
+  end
+
+  FileUtils.copy('./nfl_2017.json', "./archive/nfl_2017_#{Date.today}.json")
+  File.open('./nfl_2017.json',"w") do |f|
+    f.write(all_teams.to_json)
+  end
 end
-
-FileUtils.copy('./nfl_2017.json', "./archive/nfl_2017_#{Date.today}.json")
-File.open('./nfl_2017.json',"w") do |f|
-  f.write(all_teams.to_json)
-end
-
 
 generate_team_table
 puts '---------------------------||-------------------------'
