47ea3bfcfdf36e98cfb28eb74f3b3658b04d2629
Jeff Miller <jeffmiller00@gmail.com>
2017-11-21 18:24:16 -0600

    Change the source of the NFL data to ESPN.

--- fetch_nfl_wins.rb
+++ fetch_nfl_wins.rb
@@ -53,27 +53,24 @@ end
 
 def write_file?; true; end;
 
+request = HTTPI::Request.new
+request.url = 'http://www.espn.com/nfl/standings/_/group/league'
+# request.query = { Season: '2015-16',
+#                   SeasonType: 'Regular%20Season'}
+response = HTTPI.get(request)
+full_doc = Nokogiri::HTML(response.body)
+truth_teams = full_doc.xpath('//*[@id="main-container"]/div/section/div[2]/div/div[2]/table/tr')
+
+all_teams = File.read('./nfl_2017.json')
+all_teams = JSON.parse(all_teams)
+
+all_teams.each do |team|
+  binding.pry unless truth_teams.search("[text()*='#{team['name']}']").first
+  wins = truth_teams.search("[text()*='#{team['name']}']").first.parent.parent.parent.parent.children[1].text.to_i
+  team['wins'][Date.today.prev_day.to_s] = wins
+end
+
 if write_file?
-  request = HTTPI::Request.new
-  request.url = 'http://www.nfl.com/standings'
-  request.query = { category: 'league',
-                    season: '2017-REG',
-                    split: 'Overall',
-                    sort: 'OVERALL_WINS',
-                    order: 'desc'}
-  response = HTTPI.get(request)
-  full_doc = Nokogiri::HTML(response.body)
-
-  truth_teams = full_doc.xpath('//*[@id="main-content"]/div[1]/div[2]/div[1]/table/tr')
-
-  all_teams = File.read('./nfl_2017.json')
-  all_teams = JSON.parse(all_teams)
-
-  all_teams.each do |team|
-    wins = truth_teams.search("[text()*='#{team['name']}']").first.parent.parent.children[7].text.to_i
-    team['wins'][Date.today.prev_day.to_s] = wins
-  end
-
   FileUtils.copy('./nfl_2017.json', "./archive/nfl_2017_#{Date.today}.json")
   File.open('./nfl_2017.json',"w") do |f|
     f.write(all_teams.to_json)
