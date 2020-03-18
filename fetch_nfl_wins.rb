0d74a83bf7a104203cc17f65dcdef10d2968a9d5
Jeff Miller <jmiller@ncsasports.org>
2020-03-18 15:38:59 -0500

    Final push of changes to the draft for 2019.

--- fetch_nfl_wins.rb
+++ fetch_nfl_wins.rb
@@ -3,8 +3,10 @@ require 'nokogiri'
 require 'json'
 require 'pry'
 
+DATA_FILE = './nfl_2019.json'
+
 def get_teams
-  all_teams = File.read('./nfl_2018.json')
+  all_teams = File.read(DATA_FILE)
   JSON.parse(all_teams)
 end
 
@@ -19,13 +21,13 @@ def generate_team_table
 end
 
 EMPTY_WEEK = {jeff: 0, greg: 0, tim: 0, zach: 0, mike: 0}
-CURRENT_NFL_WEEK = 17
+CURRENT_NFL_WEEK = 2
 
 def generate_summary_chart
   all_teams = get_teams
   weeklySummary = []
-  week1begin = Date.parse('2018-09-06')
-  week1end   = Date.parse('2018-09-10')
+  week1begin = Date.parse('2019-09-03')
+  week1end   = Date.parse('2019-09-10')
   CURRENT_NFL_WEEK.times do |i|
     weeklySummary[i] = EMPTY_WEEK.dup
     weekBegin = week1begin.next_day(7*i)
@@ -68,14 +70,16 @@ if fetch_wins?
   truth_teams = full_doc.xpath('//table[@class="TableBase-table"]/tr')
 
 
-  all_teams = File.read('./nfl_2018.json')
+  all_teams = File.read(DATA_FILE)
   all_teams = JSON.parse(all_teams)
 
   all_teams.each do |team|
     if truth_teams.search("[text()*='#{team['location']}']").first
-      wins = truth_teams.search("[text()*='#{team['location']}']").first.parent.parent.parent.parent.parent.children[2].text.to_i
+      wins = truth_teams.search("[text()*='#{team['location']}']").first.parent.parent.parent.parent.parent.parent.children[2].text.to_i
+      puts "#{team['location']} | #{wins}"
     elsif truth_teams.search("[text()*='#{team['name']}']").first
-      wins = truth_teams.search("[text()*='#{team['name']}']").first.parent.parent.parent.parent.parent.children[2].text.to_i
+      wins = truth_teams.search("[text()*='#{team['name']}']").first.parent.parent.parent.parent.parent.parent.children[2].text.to_i
+      puts "#{team['location']} | #{wins}"
     else
       binding.pry
     end
@@ -84,10 +88,12 @@ if fetch_wins?
   end
 
   if write_file?
-    FileUtils.copy('./nfl_2018.json', "./archive/nfl_#{Date.today}.json")
-    File.open('./nfl_2018.json',"w") do |f|
+    FileUtils.copy(DATA_FILE, "./archive/nfl_#{Date.today}.json")
+    File.open(DATA_FILE,"w") do |f|
       f.write(all_teams.to_json)
     end
+  else
+    puts all_teams.to_json
   end
 end
 
--- upload_to_s3.sh
+++ upload_to_s3.sh
@@ -1,2 +1,6 @@
-aws s3 cp ./index.html s3://www.millerteamleague.com/
-aws s3 cp ./nba/index.html s3://www.millerteamleague.com/nba/
+# bash -c 'while [ 0 ]; do sh ./upload_to_s3.sh; sleep 0.5;done'
+
+# aws s3 cp ./index.html s3://www.millerteamleague.com/
+# aws s3 cp ./nba/draft.html s3://www.millerteamleague.com/
+aws s3 cp ./nba/nba_2019.json s3://www.millerteamleague.com/
+# aws s3 cp ./nba/index.html s3://www.millerteamleague.com/nba/
